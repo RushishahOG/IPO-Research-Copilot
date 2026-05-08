5e6e0c42ac027da1ffbd5d17698e13ce1d9f350c
RushishahOG <rushishah004@gmail.com>
2026-05-08 10:35:29 +0530

    change

--- api/main.py
+++ api/main.py
@@ -45,5 +45,5 @@ if __name__ == "__main__":
     uvicorn.run(
         "api.main:app",
         host="0.0.0.0",
-        port=int(os.environ.get("PORT", 8000)),
+        port=int(os.environ.get("PORT", 2706)),
     )
